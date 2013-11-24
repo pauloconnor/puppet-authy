4d93e5722dcf168c7884f029deec6baff079f8a8
Paul O'Connor <paul@poconnor.me>
2013-11-24 12:54:57 +0000

    Authy changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Authy.app to /Applications.
+#
+# Examples
+#
+#   include authy
+class authy {
+  package { 'Authy':
+    provider => 'compressed_app',
+    source   => 'https://www.authy.com/authy-desktop/Authy_Bluetooth-1.2.app.tar.bz2',
+  }
 }
