5489cae1518eac475a5d1011106da29c7829a193
Paul O'Connor <paul@poconnor.me>
2014-03-31 14:03:52 +0100

    Fix tests

--- spec/classes/authy_spec.rb
+++ spec/classes/authy_spec.rb
@@ -4,7 +4,7 @@ describe 'authy' do
   it do
     should contain_package('Authy').with({
       :provider => 'compressed_app',
-      :source   => 'https://www.authy.com/authy-desktop/Authy_Bluetooth-1.2.app.tar.bz2',
+      :source   => 'https://www.authy.com/authy-desktop/Authy_Bluetooth-1.3.app.zip',
     })
   end
 end
